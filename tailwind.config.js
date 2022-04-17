28b182b766a2bd9c197df27ede0f9cb5355ec43d
tejasc0 <tejasc888@gmail.com>
2022-04-17 07:27:24 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'abril': ['Abril Fatface', 'cursive'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
